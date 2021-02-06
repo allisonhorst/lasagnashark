37d093a94cbf818ffd708dfc9a08a220b354ee5c
Allison Horst <ahorst@ucsb.edu>
2021-02-06 09:59:26 -0800

    adding and_vertebrates test

---
+++ R/and_vertebrates_doc.R
@@ -0,0 +1,8 @@
+#' Mack Creek aquatic vertebrates
+#'
+#' Sizes for trout and salamanders in Mack Creek (1987 - present)
+#'
+#' @source {Enter dataset citation here}
+#' \url{Add link for data package here}
+
+"and_vertebrates"
