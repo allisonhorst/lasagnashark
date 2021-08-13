f66b28fa6ae4785ffc98958f72c6272d94ed6ec4
allisonhorst <ahorst@ucsb.edu>
2021-08-13 11:52:52 -0700

    Initial commit

---
+++ R/about_me.R
@@ -0,0 +1,15 @@
+#' Tells you something about yourself
+#'
+#' Contains one function, about_me, that prints a statement about the user
+#'
+#' @param my_name a character containing the input name
+#' @param birth_year a number containing a four digit birth year
+#' @param fav_animal a character containing an animal species
+#'
+#' @return
+#' @export
+#'
+#' @examples
+about_me <-function(my_name, birth_year, fav_animal) {
+  print(paste0("Hello, my name is ", my_name, "! I was born in ", birth_year, " and my favorite animal is a ", fav_animal ))
+}
